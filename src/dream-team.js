5dd78254705c8ba0d6a8a8577dc7f8233ff5131e
romanovich_anton <anton.romanovich21@gmail.com>
2022-04-30 23:30:17 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(arr) {
+    if(!Array.isArray(arr)) return false;
+    let res = '';
+    for (n of arr) {
+      switch(typeof n) {
+        case 'string': 
+          res += n.trim()
+              .substring(0,1)
+              .toUpperCase()
+        break;  
+      }
+    };    
+  return res ? res.split('').sort().join("") : false;
+};
 
 module.exports = {
   createDreamTeam
